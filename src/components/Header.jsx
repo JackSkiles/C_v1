a46fff8e241e8d5284b311f73b78bee4c1077bdb
Jack Skiles <jackskiles420@gmail.com>
2020-07-24 19:32:52 -0400

    created mock up church website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,10 @@ export default function Header() {
         <div className="contain">
             <div className="title"><Link className="link"><h1 className="header">Chattanooga Primitive Baptist Church</h1></Link></div>
             <div className="links">
-                <Link className="link"><h3>Home</h3></Link>
-                <Link className="link"><h3>First Time</h3></Link>
-                <Link className="link"><h3>Learn About God</h3></Link>
-                <Link className="link"><h3>About Us</h3></Link>
+                <Link className="link"><h3 className="text">Home</h3></Link>
+                <Link className="link"><h3 className="text">First Time</h3></Link>
+                <Link className="link"><h3 className="text">Learn About God</h3></Link>
+                <Link className="link"><h3 className="text">About Us</h3></Link>
                 <Link className="link"><Button><h3>Contact Us</h3></Button></Link>
             </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,45 +2,49 @@ import React from 'react';
 import './home.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Header from './Header';
+import Map from './Map'
 
 
 export default function Home() {
     return (
         <div className="main">
+            <Header />
             <div  className="carousel">
-            <Carousel>
-                    <Carousel.Item className="cIItem">
-                        <img className="d-block w-100"  id="img" src='../OnePiece.jpg' alt="First slide"></img>
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="cIItem">
-                        <img
-                            className="d-block w-100"
-                            id="img"
-                            src="../swamp.jpg"
-                            alt="Third slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="cIItem">
-                        <img
-                            className="d-block w-100"
-                            id="img"
-                            src="../swamp.jpg"
-                            alt="Third slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+                <Carousel>
+                        <Carousel.Item className="cIItem">
+                            <img className="d-block w-100"  id="img" src='../OnePiece.jpg' alt="First slide"></img>
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item className="cIItem">
+                            <img
+                                className="d-block w-100"
+                                id="img"
+                                src="../swamp.jpg"
+                                alt="Third slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item className="cIItem">
+                            <img
+                                className="d-block w-100"
+                                id="img"
+                                src="../swamp.jpg"
+                                alt="Third slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                    <div className="theMap"><Map />
+                    </div>
             </div>
         </div>
     )
