d5e7aa12fc9a247368051a232135684b297eec9c
Clay Hindman <clayhindman01@gmail.com>
2020-07-29 19:06:38 -0400

    log in page

--- src/App.js
+++ src/App.js
@@ -9,17 +9,19 @@ import Footer from './components/Footer'
 import firebase from './firebase'
 import TimesList from './components/times-list'
 import AddTimeEntryForm from './components/add-time-entry-form'
+import LogIn from './components/LogIn'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        <TimesList />  
-        <AddTimeEntryForm />
+        {/* <TimesList />  
+        <AddTimeEntryForm /> */}
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/services" exact component={Services} /> 
+          <Route path="/login" exact component={LogIn} />
         </Switch>
         <Footer />
       </Router>
